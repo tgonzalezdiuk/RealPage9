82cfdaecb29818cc3d3ad58cb013b5025192a609
tgonzalezdiuk <tamara.gsx@gmail.com>
2024-02-07 14:45:57 -0500

    02/07/24 14:45
    Finished

---
+++ comments.java
@@ -0,0 +1,16 @@
+/*****************
+*Tamara Gonzalez
+*Date: Febuary 7th, 2024 
+*Program: Java  
+*****************/
+
+public class comments
+
+{
+    public static void main (String[]args)
+    {
+        System.out.println("This is a literral print.");
+        System.out.print("Yea!");
+        System.out.println("Go Java!");
+    }
+}
